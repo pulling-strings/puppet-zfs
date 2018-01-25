4bb42884af41b50d77d9464cb93f928fc1f90e15
ronen <narkisr@gmail.com>
2018-01-25 23:59:16 +1100

    not supporting 15.04 anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,32 +4,20 @@ class zfs {
   include zfs::sync
 
   # see http://askubuntu.com/questions/691576/zfs-broken-after-upgrading-to-15-10
-  if versioncmp($::lsbdistrelease , '15.04') <= 0 {
-    $zfs_package = 'ubuntu-zfs'
+  $zfs_package = 'zfsutils-linux'
 
-    apt::ppa{'ppa:zfs-native/stable':
-    } ->
+  apt::ppa{'ppa:zfs-native/stable':
+    ensure => absent
+  }
 
-    package{$zfs_package:
-      ensure  => present,
-      require => Class['apt::update']
-    }
-  } else {
-    $zfs_package = 'zfsutils-linux'
+  -> package{'ubuntu-zfs':
+    ensure  => absent,
+    require => Class['apt::update']
+  }
 
-    apt::ppa{'ppa:zfs-native/stable':
-      ensure => absent
-    } ->
-
-    package{'ubuntu-zfs':
-      ensure  => absent,
-      require => Class['apt::update']
-    } ->
-
-    package{$zfs_package:
-      ensure  => present,
-      require => Class['apt::update']
-    }
+  -> package{$zfs_package:
+    ensure  => present,
+    require => Class['apt::update']
   }
 
   file {'/etc/modprobe.d/zfs.conf' :
@@ -39,9 +27,9 @@ class zfs {
     owner   => root,
     group   => root,
     require => Package[$zfs_package]
-  } ->
+  }
 
-  file { '/etc/udev/rules.d/91-zfs-permissions.rules':
+  -> file { '/etc/udev/rules.d/91-zfs-permissions.rules':
     ensure => file,
     mode   => '0644',
     source => 'puppet:///modules/zfs/91-zfs-permissions.rules',
